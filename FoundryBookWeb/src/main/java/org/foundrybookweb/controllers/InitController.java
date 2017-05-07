2e680005378a5c1fcb8997495d1e018fb531c8fe
suresh <sureshsep03@gmail.com>
2017-05-07 13:34:54 +0530

    Initial Commit

---
+++ FoundryBookWeb/src/main/java/org/foundrybookweb/controllers/InitController.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.foundrybookweb.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.foundrybookservice.utils.AppUtil;
+
+/**
+ *
+ * @author skumar
+ */
+@EnableWebMvc
+@RestController
+public class InitController {
+    
+}
