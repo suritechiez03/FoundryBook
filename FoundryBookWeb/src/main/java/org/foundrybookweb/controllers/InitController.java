ff6829c4a33ecc798534f6d8bfb42a1204c33b63
suresh <sureshsep03@gmail.com>
2017-07-09 11:56:00 +0530

    spring security

--- FoundryBookWeb/src/main/java/org/foundrybookweb/controllers/InitController.java
+++ FoundryBookWeb/src/main/java/org/foundrybookweb/controllers/InitController.java
@@ -8,13 +8,30 @@ package org.foundrybookweb.controllers;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.foundrybookservice.utils.AppUtil;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
  * @author skumar
  */
+@EnableWebSecurity
 @EnableWebMvc
-@RestController
+@Controller
 public class InitController {
-    
+    @RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
+	public ModelAndView welcomePage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Hello World");
+		model.addObject("message", "This is welcome page!");
+		model.setViewName("index");
+		return model;
+
+	}
+
 }
